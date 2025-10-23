42e5367699c51f8e9295701af70087caa4a9a685
Carlos-Sardenha <sardenhacarlos@gmail.com>
2025-10-23 15:45:41 -0300

    workshop cdi

---
+++ meu-portfolio/src/App.jsx
@@ -0,0 +1,20 @@
+import Header from "./Header.jsx";
+import Sobre from "./Sobre.jsx";
+import Projetos from "./Projeto.jsx";
+import Contato from "./Contato.jsx";
+import Footer from "./Footer.jsx";
+import "./index.css"
+
+function App(){
+  return(
+    <>
+    <Header/>
+    <Sobre/>
+    <Projetos/>
+    <Contato/>
+    <Footer/>
+    </>
+  )
+}
+
+export default App;
---
+++ meu-portfolio/src/Contato.jsx
@@ -0,0 +1,15 @@
+function Contato() {
+    return (
+      <section className="contato">
+        <h2>Contato</h2>
+        <p>Me encontre nas redes sociais</p>
+        <div className="links-contato">
+          <a href="https://www.linkedin.com/in/seu-usuario" target="_blank" className="link linkedin"> LinkedIn </a>
+          <a href="https://github.com/seu-usuario" target="_blank" className="link github"> GitHub </a>
+          <a href="https://www.instagram.com/seu-usuario" target="_blank" className="link instagram" > Instagram </a>
+        </div>
+      </section>
+    );
+  }
+  
+  export default Contato;
